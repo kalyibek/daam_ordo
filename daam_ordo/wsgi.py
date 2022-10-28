a73f00194362bad9589dcb4369d9365dd7add520
kalyibek <kkalyibek@gmail.com>
2022-10-28 21:37:46 +0600

    initial

---
+++ daam_ordo/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for daam_ordo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daam_ordo.settings')
+
+application = get_wsgi_application()
