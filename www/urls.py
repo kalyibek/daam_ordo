72cf3d80e6ffbeea463810b5b51cead51b1d2b91
Sam-Po <samat.baygeldiev@alatoo.edu.kg>
2022-10-29 01:42:26 +0600

    user auth

--- www/urls.py
+++ www/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.sign_user, name='sign_user'),
+    path('logout/', views.sign_out, name='sign_out'),
+
+
 ]
--- www/views.py
+++ www/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from . import models
 
 
@@ -8,3 +10,26 @@ def index(request):
         'foods': foods
     }
     return render(request, 'index.html', context)
+
+
+def sign_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user is None:
+            return HttpResponse('Invalid credentials.')
+        login(request, user)
+        return redirect('index')
+
+    else:
+        return render(request, 'login.html')
+
+
+def sign_out(request):
+    logout(request)
+    return redirect('index')
